030118428ec60004b0d1c0ecfe58f1b67d83a616
MattJohnson24 <matt.r.johnson24@gmail.com>
2022-03-18 15:18:06 -0400

    #68 progress

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,3 +20,18 @@ const userSchema = mongoose.Schema({
 
 
 module.exports = mongoose.model('User', userSchema)
+
+const forgotSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+    },
+    token: {
+        type: String,
+        required: [true, 'Please add a token'],
+    }, 
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('forgotUser', forgotSchema)
