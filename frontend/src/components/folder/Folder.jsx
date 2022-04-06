b71505f7410600a56b5edb9b62f9ba9f48137a3b
mraddtab <70644931+mraddtab@users.noreply.github.com>
2022-04-06 00:33:56 -0400

    Folder Page Frontend
    Updated the CSS for the folder page and working on adding functionality to the create new document button.

--- frontend/src/components/folder/Folder.jsx
+++ frontend/src/components/folder/Folder.jsx
@@ -1,41 +1,101 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-
-const Folder = () => {
-
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        navigate('/login')
-    };
-
-    return (
-        <div>
-            <h1>This is the folder page</h1>
-            <h1>You are here because you are user</h1>
-            <button onClick={handleLogout}>Logout</button>
-        </div>
-    );
-}
-export default Folder;
-
-/*
-import React from "react";
-import CreateDocument from "./createDocument";
-import Document from "./document";
-
-class Folder extends Component {
-    state = {  
-        documents: [
-            {id: 1, name:"CSE442"},
-            {id: 2, name:"CSE116"}
-        ]
-    } 
+import { Component } from "react";
+import "./folder.css"
+class Menu extends Component {
     render() { 
         return (
-            <div className = "folder">
-                {this.state.documents.map(document => (<Document key = {document.id} name = {document.name}> </Document>) )}
+            <ul className="menuList">
+                <li className="menuItem">Rename</li>
+                <li className="menuItem">Open</li>
+                <li className="menuItem">Remove</li>
+                <li className="menuItem">Share</li>
+            </ul>
+        );
+    }
+}
+class CreateDocumentCircle extends Component {
+    render(){
+        return(
+            <div className = "createDocumentCircleContainer">
+                <span className = "createDocumentCirclePlus">+</span>
             </div>
         );
     }
 }
-*/
+class Document extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: "Initial State"
+        }
+    }
+    render() { 
+        return (
+            <div className = "document">
+                <div className = "documentCard" >  
+                    <img src={require("./file.png")} className = "documentImage" alt="document image"  />
+                    <img  src={require("./hamburger.png")} className = "documentHamburger" onClick = {() => alert('hello')} alt="" />
+                </div>
+                <h1 className="documentName"> {this.props.name} </h1>
+            </div>
+        );
+    }
+}
+
+class CreateDocument extends Component {
+    handleClick(){
+    }
+    render() { 
+        return (
+            <div className='createDocumentCard' onClick = {this.handleClick}>
+                <img className = "createDocumentImage" src={require("./newFile.png")}  alt="" />
+            </div>
+            
+        );
+    }
+}
+
+class NameNewDocument extends Component  {
+    render(){
+        return(
+            <div className= "newDocumentLayer">
+                <form className = "newDocumentForm" action="" >
+                    <input className = "newDocumentInput" type="text" placeholder="Document Name ... "/>
+                    <div className="newDocumentButtons"> 
+                        <button className="newDocumentCancel">Cancel</button> 
+                        <button className="newDocumentCreate">Create</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+class Folder extends Component {
+    constructor(props) {
+        super(props)
+    }
+    prependDocument(name){
+        this.setState(this.state.documents.unshift({name}))
+    }
+    state = {  
+        documents: [
+            {name:"CSE442"},
+            {name:"CSE116"},
+            {name: "CSE 306"}
+        ]
+    } 
+    render() { 
+        return (
+            <div className = "folder" >
+                <CreateDocument></CreateDocument>
+                <NameNewDocument></NameNewDocument>
+                
+              {/* {this.state.documents.map(document => (<Document name={document.name}> </Document>) )} */}
+  
+            </div>
+        );
+    }
+}
+ 
+export default Folder;
