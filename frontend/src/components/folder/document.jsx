1237391849523a9b6479352ec8ba8f4809dc35c4
mraddtab <70644931+mraddtab@users.noreply.github.com>
2022-04-13 19:27:45 -0400

    Folder page frontend

--- frontend/src/components/folder/document.jsx
+++ frontend/src/components/folder/document.jsx
@@ -4,25 +4,38 @@ import Menu from "./menu";
 import "./folder.css"
 
 class Document extends Component {
+
+
+
     constructor(props) {
         super(props)
         this.state = {
-            name: "Initial State"
+            showMenu: false
+        }
+        this.handleMenuClick = this.handleMenuClick.bind(this)
+    }
+    
+    renderDropdownMenu(){
+        if(this.state.showMenu === true){
+            return (
+            <Menu></Menu>
+            )
         }
     }
-    renderDropdownMenu(){
-        return(
-            <Menu></Menu>
-        );
+    handleMenuClick(){
+        this.setState({
+            showMenu: !this.state.showMenu
+        })
     }
     render() { 
         return (
             <div className = "document">
                 <div className = "documentCard" >  
                     <img src={require("./file.png")} className = "documentImage" alt="document image"  />
-                    <img  src={require("./hamburger.png")} className = "documentHamburger" onClick = {this.renderDropdownMenu} alt="" />
+                    <img  src={require("./hamburger.png")} className = "documentHamburger" onClick = {this.handleMenuClick} alt="" />
                 </div>
                 <h1 className="documentName"> {this.props.name} </h1>
+                {this.renderDropdownMenu()}
             </div>
         );
     }
