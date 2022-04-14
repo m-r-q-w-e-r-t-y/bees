8075b457fcffac9fcea5952f94b58670ed75fc55
mraddtab <70644931+mraddtab@users.noreply.github.com>
2022-04-14 18:09:19 -0400

    Folder page rename documents

--- frontend/src/components/folder/document.jsx
+++ frontend/src/components/folder/document.jsx
@@ -2,26 +2,42 @@ import React from "react";
 import { Component } from "react";
 import Menu from "./menu";
 import "./folder.css"
+import RenameDocument from "./renameDocument";
 
 class Document extends Component {
-
-
 
     constructor(props) {
         super(props)
         this.state = {
-            showMenu: false
+            showMenu: false,
+            renaming: false
         }
         this.handleMenuClick = this.handleMenuClick.bind(this)
+        this.cancelNaming = this.cancelNaming.bind(this)
     }
     
-    renderDropdownMenu(){
-        if(this.state.showMenu === true){
-            return (
-            <Menu></Menu>
+    renderRenameForm(){
+        if(this.state.renaming == true){
+            return(
+                <RenameDocument cancelHandler = {this.cancelNaming}></RenameDocument>   
             )
         }
     }
+
+    renderDropdownMenu(){
+        if(this.state.showMenu === true){
+            return (
+                <Menu document = {this.props.name} ></Menu>
+            )
+        }
+    }
+
+    cancelNaming(){
+        this.setState({
+            renaming:false
+        })
+    }
+
     handleMenuClick(){
         this.setState({
             showMenu: !this.state.showMenu
@@ -33,9 +49,12 @@ class Document extends Component {
                 <div className = "documentCard" >  
                     <img src={require("./file.png")} className = "documentImage" alt="document image"  />
                     <img  src={require("./hamburger.png")} className = "documentHamburger" onClick = {this.handleMenuClick} alt="" />
+ 
                 </div>
+                    {this.renderDropdownMenu()}
+                    {this.renderRenameForm()}
                 <h1 className="documentName"> {this.props.name} </h1>
-                {this.renderDropdownMenu()}
+
             </div>
         );
     }
